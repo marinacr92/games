c341f812f5f4a0339f1838f36ff713a0e556cd0c
marinacr92 <marinacaro92@gmail.com>
2023-06-09 13:22:00 +0200

    Add winner checks

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,145 +1,224 @@
 /* eslint-disable default-case */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const TicTacToe = () => {
-  const [A1, setA1] = useState(false);
-  const [A2, setA2] = useState(false);
-  const [A3, setA3] = useState(false);
-  const [B1, setB1] = useState(false);
-  const [B2, setB2] = useState(false);
-  const [B3, setB3] = useState(false);
-  const [C1, setC1] = useState(false);
-  const [C2, setC2] = useState(false);
-  const [C3, setC3] = useState(false);
-
   const [count, setCount] = useState(0);
 
-  const [infoA1, setInfoA1] = useState('');
-  const [infoA2, setInfoA2] = useState('');
-  const [infoA3, setInfoA3] = useState('');
-  const [infoB1, setInfoB1] = useState(false);
-  const [infoB2, setInfoB2] = useState(false);
-  const [infoB3, setInfoB3] = useState(false);
-  const [infoC1, setInfoC1] = useState(false);
-  const [infoC2, setInfoC2] = useState(false);
-  const [infoC3, setInfoC3] = useState(false);
+  const [selectedCell, setSelectedCell] = useState([
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+  ]);
 
+  const [player, setPlayer] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]);
 
-  const counter = () => {
-    setCount(count + 1)
+  const [winnerName, setWinnerName] = useState('');
+
+  const isSelected = (x, y) => {
+    selectedCell[x][y] = true;
+    setSelectedCell([...selectedCell]);
   };
 
-  const player = (setInfo) => {
+  const counter = () => {
+    setCount(count + 1);
+  };
+
+  const whoPlays = (x, y) => {
     if (count % 2 === 0) {
-      setInfo('even')
+      player[x][y] = 'even';
     } else {
-      setInfo('odd');
+      player[x][y] = 'odd';
     }
-  }
+    setPlayer([...player]);
+  };
 
   const handleClick = (ev) => {
     switch (ev.target.id) {
       case 'A1':
         counter();
-        setA1(true);
-        player(setInfoA1);
+        isSelected(0, 0);
+        whoPlays(0, 0);
         break;
       case 'A2':
-        setA2(true);
+        isSelected(0, 1);
         counter();
-        player(setInfoA2);
+        whoPlays(0, 1);
         break;
       case 'A3':
-        setA3(true);
+        isSelected(0, 2);
         counter();
-        player(setInfoA3);
+        whoPlays(0, 2);
         break;
       case 'B1':
-        setB1(true);
+        isSelected(1, 0);
         counter();
-        player(setInfoB1);
+        whoPlays(1, 0);
         break;
       case 'B2':
-        setB2(true);
+        isSelected(1, 1);
         counter();
-        player(setInfoB2);
+        whoPlays(1, 1);
         break;
       case 'B3':
-        setB3(true);
+        isSelected(1, 2);
         counter();
-        player(setInfoB3);
+        whoPlays(1, 2);
         break;
       case 'C1':
-        setC1(true);
+        isSelected(2, 0);
         counter();
-        player(setInfoC1);
+        whoPlays(2, 0);
         break;
       case 'C2':
-        setC2(true);
+        isSelected(2, 1);
         counter();
-        player(setInfoC2);
+        whoPlays(2, 1);
         break;
       case 'C3':
-        setC3(true);
+        isSelected(2, 2);
         counter();
-        player(setInfoC3);
+        whoPlays(2, 2);
         break;
     }
   };
-  const handleCLickNone = () => {
 
-  }
+  const handleCLickNone = () => {};
 
-  const classColor = (infoZ0) => {
-    if (infoZ0 === "even") {
-      return "blue"
-    } else if (infoZ0 === "odd") {
-      return "red"
+  useEffect(() => {
+    if (
+      player[0][0] === player[0][1] &&
+      player[0][1] === player[0][2] &&
+      player[0][0] !== ''
+    ) {
+      setWinnerName(player[0][0]);
+    } else if (
+      player[1][0] === player[1][1] &&
+      player[1][1] === player[1][2] &&
+      player[1][0] !== ''
+    ) {
+      setWinnerName(player[1][0]);
+    } else if (
+      player[2][0] === player[2][1] &&
+      player[2][1] === player[2][2] &&
+      player[2][0] !== ''
+    ) {
+      setWinnerName(player[2][0]);
+    } else if (
+      player[0][0] === player[1][0] &&
+      player[1][0] === player[2][0] &&
+      player[0][0] !== ''
+    ) {
+      setWinnerName(player[0][0]);
+    } else if (
+      player[0][1] === player[1][1] &&
+      player[1][1] === player[2][1] &&
+      player[0][1] !== ''
+    ) {
+      setWinnerName(player[0][1]);
+    } else if (
+      player[0][2] === player[1][2] &&
+      player[1][2] === player[2][2] &&
+      player[0][2] !== ''
+    ) {
+      setWinnerName(player[0][2]);
+    } else if (
+      player[0][0] === player[1][1] &&
+      player[1][1] === player[2][2] &&
+      player[0][0] !== ''
+    ) {
+      setWinnerName(player[0][0]);
+    } else if (
+      player[0][2] === player[1][1] &&
+      player[1][1] === player[2][0] &&
+      player[0][2] !== ''
+    ) {
+      setWinnerName(player[0][2]);
     }
-  }
+  }, [player]);
 
   return (
     <>
-      <table border="5" >
+      <table border="5">
         <tr>
-          <td onClick={A1 === false ? handleClick : handleCLickNone} id="A1" className={`table ${classColor(infoA1)}`}>
-            A1
-          </td>
-          <td id="A2" className={`table ${classColor(infoA2)}`}
-            onClick={A2 === false ? handleClick : handleCLickNone}
-          >
-            A2
-          </td>
-          <td id="A3" className={`table ${classColor(infoA3)}`}
-            onClick={A3 === false ? handleClick : handleCLickNone}
-          >
-            A3
-          </td>
+          <td
+            onClick={
+              selectedCell[0][0] === false ? handleClick : handleCLickNone
+            }
+            id="A1"
+            className={`table ${player[0][0]}`}
+          ></td>
+          <td
+            id="A2"
+            className={`table ${player[0][1]}`}
+            onClick={
+              selectedCell[0][1] === false ? handleClick : handleCLickNone
+            }
+          ></td>
+          <td
+            id="A3"
+            className={`table ${player[0][2]}`}
+            onClick={
+              selectedCell[0][2] === false ? handleClick : handleCLickNone
+            }
+          ></td>
         </tr>
         <tr>
-          <td id="B1" onClick={B1 === false ? handleClick : handleCLickNone} className={`table ${classColor(infoB1)}`}>
-            B1
-          </td>
-          <td id="B2" onClick={B2 === false ? handleClick : handleCLickNone} className={`table ${classColor(infoB2)}`}>
-            B2
-          </td>
-          <td id="B3" onClick={B3 === false ? handleClick : handleCLickNone} className={`table ${classColor(infoB3)}`}>
-            B3
-          </td>
+          <td
+            id="B1"
+            onClick={
+              selectedCell[1][0] === false ? handleClick : handleCLickNone
+            }
+            className={`table ${player[1][0]}`}
+          ></td>
+          <td
+            id="B2"
+            onClick={
+              selectedCell[1][1] === false ? handleClick : handleCLickNone
+            }
+            className={`table ${player[1][1]}`}
+          ></td>
+          <td
+            id="B3"
+            onClick={
+              selectedCell[1][2] === false ? handleClick : handleCLickNone
+            }
+            className={`table ${player[1][2]}`}
+          ></td>
         </tr>
         <tr>
-          <td id="C1" onClick={C1 === false ? handleClick : handleCLickNone} className={`table ${classColor(infoC1)}`}>
-            C1
-          </td>
-          <td id="C2" onClick={C2 === false ? handleClick : handleCLickNone} className={`table ${classColor(infoC2)}`}>
-            C2
-          </td>
-          <td id="C3" onClick={C3 === false ? handleClick : handleCLickNone} className={`table ${classColor(infoC3)}`}>
-            C3
-          </td>
+          <td
+            id="C1"
+            onClick={
+              selectedCell[2][0] === false ? handleClick : handleCLickNone
+            }
+            className={`table ${player[2][0]}`}
+          ></td>
+          <td
+            id="C2"
+            onClick={
+              selectedCell[2][1] === false ? handleClick : handleCLickNone
+            }
+            className={`table ${player[2][1]}`}
+          ></td>
+          <td
+            id="C3"
+            onClick={
+              selectedCell[2][2] === false ? handleClick : handleCLickNone
+            }
+            className={`table ${player[2][2]}`}
+          ></td>
         </tr>
       </table>
       <h2>{count}</h2>
+      <h2 className={winnerName === '' ? 'hidden' : ''}>
+        {winnerName === 'even'
+          ? 'Ha ganado el Jugador 1'
+          : 'Ha ganado el Jugador 2'}
+      </h2>
     </>
   );
 };
