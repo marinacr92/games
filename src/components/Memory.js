b56e5200882fe3cfd61ac215c51a893cc0e937f0
marinacr92 <marinacaro92@gmail.com>
2023-06-20 11:06:48 +0200

    Update flipCard function

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -30,18 +30,17 @@ const Memory = () => {
     CaptainDuck,
   ]);
 
-  const [foundPair, setFoundPair] = useState([]);
+  const [cardsFlipped, setCardsFlipped] = useState([]);
+  const [cardsMatched, setCardsMatched] = useState([]);
 
-  const [isSelected, setIsSelected] = useState([]);
+  //const [isSelected, setIsSelected] = useState([]);
 
-  const [click1, setClick1] = useState('');
-  const [click2, setClick2] = useState('');
+  //const [click1, setClick1] = useState('');
+  //const [click2, setClick2] = useState('');
 
   // const [isActived, setIsActived] = useState('');
 
   const [counter, setCounter] = useState(0);
-
-
 
   const shuffle = () => {
     images.sort(() => {
@@ -50,80 +49,108 @@ const Memory = () => {
     setImages([...images]);
   };
 
-  useEffect(
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    shuffle, []
-  )
+  // useEffect(
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   shuffle(),
+  //   []
+  // );
 
-  useEffect(
-    () => {
-      if (click1 === click2 && click1 !== '') {
-        setFoundPair([...foundPair, click1])
+  const flipCards = (cardId) => {
+    if (
+      cardsFlipped.length < 2 &&
+      !cardsFlipped.includes(cardId) &&
+      !cardsMatched.includes(cardId)
+    ) {
+      const newCardsFlipped = [...cardsFlipped, cardId];
+      setCardsFlipped(newCardsFlipped);
+
+      if (newCardsFlipped.length === 2) {
+        const card1 = newCardsFlipped[0];
+        const card2 = newCardsFlipped[1];
+        console.log('card1', card1, 'card2', card2);
+
+        if (images[card1] === images[card2]) {
+          setCardsMatched([...cardsMatched, card1, card2]);
+
+          setCardsFlipped([]);
+        } else {
+          setTimeout(() => {
+            setCardsFlipped([]);
+          }, 1000);
+        }
       }
-      if (click1 !== '' && click2 !== '') {
-        setClick1('');
-        setClick2('');
-        // noSelected()
-      }
-    }, [click1, click2]
-  )
+    }
+  };
 
+  // useEffect(() => {
+  //   if (click1 === click2 && click1 !== '') {
+  //     setFoundPair([...foundPair, click1]);
+  //   }
+  //   if (click1 !== '' && click2 !== '') {
+  //     setClick1('');
+  //     setClick2('');
+  //     // noSelected()
+  //   }
+  // }, [click1, click2]);
 
-  const increment = () => {
-    setCounter(counter + 1)
-  }
+  // //const increment = () => {
+  //   setCounter(counter + 1);
+  // };
   // const noSelected = () => {
   //   setIsActived('')
   // }
   // const active = () => {
   //   setIsActived('selected')
   // }
-  const saveImg = (card) => {
-    if (click1 === '') {
-      setClick1(card);
-    } else if (click2 === '') {
-      setClick2(card);
-      increment()
+  // const saveImg = (card) => {
+  //   if (click1 === '') {
+  //     setClick1(card);
+  //   } else if (click2 === '') {
+  //     setClick2(card);
+  //     increment();
+  //   }
+  // };
+
+  // const selectCard = (index) => {
+  //   setIsSelected([...isSelected, index]);
+  //   if (isSelected.length === 2) {
+  //     setIsSelected([]);
+  //   }
+  // };
+
+  const winner = () => {
+    if (cardsMatched.length === 8) {
+      return 'Has ganado';
     }
   };
 
-  const selectCard = (index) => {
-    setIsSelected([...isSelected, index])
-    if (isSelected.length === 2) {
-      setIsSelected([])
-    }
-  }
-
-  const winner = () => {
-    if (foundPair.length === 8) {
-      return 'Has ganado'
-    }
-  }
-
-
-
   const handleReset = () => {
-    setFoundPair([]);
-    setClick1('');
-    setClick2('');
+    setCardsMatched([]);
+    //setClick1('');
+    //setClick2('');
     shuffle();
     setCounter(0);
-  }
+  };
 
   return (
     <>
       <div className="board">
         <MemoryCards
           images={images}
-          saveImg={saveImg}
-          foundPair={foundPair}
-          selectCard={selectCard}
-          isSelected={isSelected}
-        // active={active}
-        // isActived={isActived}
+          //saveImg={saveImg}
+          cardsMatched={cardsMatched}
+          cardsFlipped={cardsFlipped}
+          flipCards={flipCards}
+          //selectCard={selectCard}
+          //isSelected={isSelected}
+          // active={active}
+          // isActived={isActived}
         />
       </div>
-      <button onClick={handleReset}> {counter === 0 ? 'Play!' : 'Reset'}</button>
+      <button onClick={handleReset}>
+        {' '}
+        {counter === 0 ? 'Play!' : 'Reset'}
+      </button>
       <p>{counter}</p>
       <h3>{winner()}</h3>
     </>
--- src/components/MemoryCards.js
+++ src/components/MemoryCards.js
@@ -1,12 +1,19 @@
-const MemoryCards = ({ images, saveImg, foundPair, selectCard, isSelected }) => {
-
-
-  const rotate = (card, index) => {
-    saveImg(card);
-    selectCard(index);
-    // active()
-    // ev.currentTarget.className += ' ' + 'selected'
-  }
+const MemoryCards = ({
+  images,
+  //saveImg,
+  //foundPair,
+  //selectCard,
+  //isSelected,
+  flipCards,
+  cardsFlipped,
+  cardsMatched,
+}) => {
+  // const rotate = (card, index) => {
+  //   saveImg(card);
+  //   selectCard(index);
+  //   // active()
+  //   // ev.currentTarget.className += ' ' + 'selected'
+  // };
 
   const eachCard = images.map((card, index) => {
     return (
@@ -15,21 +22,31 @@ const MemoryCards = ({ images, saveImg, foundPair, selectCard, isSelected }) => 
       //     className={`card__img ${foundPair.includes(card) ? 'hidden' : ''}`}
       //     onClick={() => saveImg(card)} />
       // </figure>
-      <div key={index} id={index} onClick={(ev) => rotate(card, index)}
+      <div
+        key={index}
+        //id={index}
+        onClick={() => flipCards(index)}
         className={`card 
-         ${isSelected.includes(index) ? 'selected' : ''} ${foundPair.includes(card) ? 'selected' : ''}
+         ${cardsFlipped.includes(index) ? 'flipped' : ''} ${
+          cardsMatched.includes(index) ? 'flipped' : ''
+        }
         `}
-      // ${isActived ? 'selected' : ''}`}
+        // ${isActived ? 'selected' : ''}`}
       >
-
-        <figure className="card__front card__face" >
-          <img src='https://st3.depositphotos.com/4431055/16834/i/450/depositphotos_168348748-stock-photo-yellow-rubber-ducks-water-background.jpg' alt='Cara A'
-            className='card__face--img' />
+        <figure className="card__front card__face">
+          <img
+            src="https://st3.depositphotos.com/4431055/16834/i/450/depositphotos_168348748-stock-photo-yellow-rubber-ducks-water-background.jpg"
+            alt="Cara A"
+            className="card__face--img"
+          />
         </figure>
-        <figure className="card__back card__face" >
-          <img id={card} src={card} alt="Cara B"
-            className='card__face--img'
-          // onClick={() => saveImg(card)} 
+        <figure className="card__back card__face">
+          <img
+            id={card}
+            src={card}
+            alt="Cara B"
+            className="card__face--img"
+            // onClick={() => saveImg(card)}
           />
         </figure>
       </div>
